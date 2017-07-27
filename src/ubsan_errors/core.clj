af7f636380cb0e4731b691808cb4e3663cb28c92
Arthur Edelstein <arthuredelstein@gmail.com>
2017-07-26 23:19:57 -0700

    clean up

--- src/ubsan_errors/core.clj
+++ src/ubsan_errors/core.clj
@@ -4,30 +4,42 @@
 
 (def ^:dynamic *results* "../download_taskcluster_logs/results/")
 
-(defn file-line-seq [path]
+(defn file-line-seq
+  "Returns a seq of the lines in the file at path."
+  [path]
   (line-seq (io/reader path)))
 
-(defn log-files [path]
+(defn list-files
+  "Get the list of files found in directory at path."
+  [path]
   (->> path
        io/as-file
        .listFiles
        seq))
 
-(defn select-runtime-error-lines [lines]
+(defn select-runtime-error-lines
+  "Select lines with the string 'runtime error:'."
+  [lines]
   (filter #(.contains % "runtime error:") lines))
 
-(defn extract-runtime-error [line]
+(defn extract-runtime-error
+  "For a 'runtime error:' line, extract ['file:row:col' 'error message']."
+  [line]
   (try
     (->
      (re-find #"(\S+): runtime error: (.*?$)" line)
      (subvec 1))
     (catch Exception e
-      (println "failed to extract: " line)
-      nil)))
+      (println line)
+      ;; A few runtime errors report no source line, oddly.
+      (->
+       (re-find #"runtime error: (.*?$)" line)
+       (assoc 0 nil)))))
 
-(defn runtime-errors []
-  (->> (log-files *results*)
+(defn runtime-errors
+  "Extract all runtime errors in all the log files."
+  []
+  (->> (list-files *results*)
        (mapcat file-line-seq)
        (select-runtime-error-lines)
-       (map extract-runtime-error)
-       (remove nil?)))
+       (map extract-runtime-error)))
