835f769bdf7e2de38223c149203e096aabfa3dce
Arthur Edelstein <arthuredelstein@gmail.com>
2017-08-01 11:36:43 -0700

    add logs.clj

--- src/ubsan_errors/core.clj
+++ src/ubsan_errors/core.clj
@@ -1,7 +1,7 @@
 (ns ubsan-errors.core
   (:require [clojure.java.io :as io]))
 
-(def ^:dynamic *results* "../download_taskcluster_logs/results/")
+(def ^:dynamic *results* "./logs/")
 
 (defn fmap
   "Map function f over values of map m."
@@ -32,13 +32,12 @@
   (try
     (->
      (re-find #"(\S+): runtime error: (.*?$)" line)
-     (subvec 1))
+     (subvec 1 3))
     (catch Exception e
-      (println line)
       ;; A few runtime errors report no source line, oddly.
-      (->
-       (re-find #"runtime error: (.*?$)" line)
-       (assoc 0 nil)))))
+      (vector
+       nil
+       (second (re-find #"runtime error: (.*?$)" line))))))
 
 (defn runtime-errors
   "Extract all runtime errors in all the log files."
@@ -48,20 +47,27 @@
        (select-runtime-error-lines)
        (map extract-runtime-error)))
 
-(defn print-summary [runtime-errs]
+(defn print-summary
+  "Print a table of rows with
+   [runtime error location, count, typical error message]"
+  [runtime-errs]
   (let [;; group by file and line number
         resf (group-by first runtime-errs)
         ;; clean up redundant data
         resfs (fmap resf #(map second %))
         ;; produce [file:line:col [number representative-err-msg]]
-        resfsr (fmap resfs (juxt count first))]
-    (doseq [[line [c m]] resfsr]
+        resfsr (for [[line mesgs] resfs]
+                 [line (count mesgs) (first mesgs)])]
+    (doseq [[line c m] (reverse (sort-by second resfsr))]
       (let [line2 (when line
                     (.replace line
                               "/home/worker/workspace/build/src/" " "))]
         (println line2 "\t" c "\t" m)))))
 
-; How to use:
-; (def res (runtime-errors))
-; (def summary-file (io/writer "ubsan-error-summary.txt"))
-; (binding [*out* summary-file] (print-summary res))
+(defn save-summary
+  "Save a table of rows with
+  [runtime error location, count typical error message]"
+  [runtime-errs]
+  (spit "firefox-ubsan-error-summary.txt"
+        (with-out-str
+          (print-summary runtime-errs))))
---
+++ src/ubsan_errors/logs.clj
@@ -0,0 +1,74 @@
+(ns ubsan-errors.logs
+  (:require [clojure.data.json :as json]
+            [clj-http.client :as http]))
+
+(def taskGroupId "cz1GoajhTM29AKwxMsHqSw")
+
+(defn task-group-url
+  "A url for listing the task group. Optional
+   continuation token (otherwise provide nil)."
+  [continuation-token]
+  (str "https://queue.taskcluster.net/v1/task-group/"
+       taskGroupId "/list"
+       (if continuation-token
+         (str "?continuationToken=" continuation-token)
+         "")))
+
+(defn fetch-tasks
+  "Fetch all tasks from the task group."
+  []
+  (loop [continuation-token nil tasks '()]
+    (let [url (task-group-url continuation-token)
+          raw (:body (http/get url))
+          data (json/read-str raw :key-fn keyword)
+          tasks+ (concat tasks (data :tasks))
+          new-continuation-token (data :continuationToken)]
+      (if new-continuation-token
+        (recur new-continuation-token tasks+)
+        tasks+))))
+
+(defn task-log-url
+  "A URL for downloading the log for a given task with
+   a 'run' index."
+  [task-id run]
+  (str "https://public-artifacts.taskcluster.net/"
+       task-id "/" run
+       "/public/logs/live_backing.log"))
+
+(defn task-log-url2 [task-id run]
+  "A second possible URL for downloading the log for
+   the given task with a 'run' index."
+  (str "https://queue.taskcluster.net/v1/task/" task-id
+       "/runs/" run
+       "/artifacts/public/logs/live_backing.log.gz"))
+
+(defn get-body
+  "Attempt to download contents at a URL and
+   return nil if it fails."
+  [url]
+  (try
+    (:body (http/get url))
+    (catch Exception e nil)))
+
+(defn download-task-log!
+  "Download the log file for a single task."
+  [task]
+  (let [task-status (task :status)
+        task-state (task-status :state)
+        task-id (task-status :taskId)
+        runs-count (count (task-status :runs))
+        last-run (dec runs-count)]
+    (println task-id task-state)
+    (if (= task-state "exception")
+      (println task-state last-run)
+      (let [body (or (get-body (task-log-url task-id last-run))
+                     (get-body (task-log-url2 task-id last-run)))]
+        (when (nil? body)
+          (throw (Exception. "download failed.")))
+        (spit (str "./logs/" task-id) body)))))
+
+(defn download-task-logs!
+  "Download the log files for all tasks in the tasks group."
+  []
+  (dorun (map download-task-log! (fetch-tasks))))
+
